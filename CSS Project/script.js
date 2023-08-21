6f1dcbf7211c309cd464f052e469c0d0c7d15e3e
Bayya Reddy Danduri <87760541+D-BayyaReddy@users.noreply.github.com>
2023-08-22 01:27:03 +0530

    Add files via upload

---
+++ CSS Project/script.js
@@ -0,0 +1,8 @@
+{/*  */}
+
+{/* <lord-icon
+    src="https://cdn.lordicon.com/rfnfarqt.json"
+    trigger="hover"
+    colors="outline:#121331,primary:#b26836,secondary:#ffc738"
+    style="width:250px;height:250px">
+</lord-icon> */}
